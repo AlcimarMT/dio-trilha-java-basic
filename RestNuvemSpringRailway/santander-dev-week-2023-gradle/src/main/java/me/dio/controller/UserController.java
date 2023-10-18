71fa6f89411ab7b4a2f624d27fd2fba7ce0fa9c9
AlcimarMT <alcimartri1998@gmail.com>
2023-10-17 22:56:33 -0300

    Conectando com o railway

---
+++ RestNuvemSpringRailway/santander-dev-week-2023-gradle/src/main/java/me/dio/controller/UserController.java
@@ -0,0 +1,44 @@
+package me.dio.controller;
+
+
+import me.dio.domain.model.User;
+import me.dio.service.UserService;
+import me.dio.service.impl.UserServiceImpl;
+import org.apache.coyote.Response;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<User> create(@RequestBody User userToCreate){
+        User userCreated = userService.create(userToCreate);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreated.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreated);
+    }
+
+}
